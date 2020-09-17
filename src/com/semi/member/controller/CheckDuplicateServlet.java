c48a8f07c595b06f82ecffd37540a1aedc0d86f7
HJ_Yoo <HY_Yoo@DESKTOP-7ADTNUI>
2020-09-17 20:12:02 +0900

    [HJ]04_중복파일로 덮어쓰기

---
+++ src/com/semi/member/controller/CheckDuplicateServlet.java
@@ -0,0 +1,53 @@
+package com.semi.member.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.semi.member.model.service.MemberService;
+import com.semi.member.model.vo.Member;
+
+/**
+ * Servlet implementation class CheckDuplicateServlet
+ */
+@WebServlet("/checkIdDuplicate")
+public class CheckDuplicateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CheckDuplicateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//클라이언트가 보내준 id값을 가져와서 
+		//DB에 있는 값인지 확인하고 결과를 페이지에 전달하는 기능
+		String MemberId=request.getParameter("id");
+			
+		Member m=new MemberService().selectMember(MemberId);
+		System.out.println(m);
+		request.setAttribute("result", m);
+				
+		request.getRequestDispatcher("/views/member/checkIdDuplicate.jsp")
+		.forward(request,response);	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
