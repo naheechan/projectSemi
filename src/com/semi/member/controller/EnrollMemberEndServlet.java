ec41e58c77fd914edc58dd5e355952e750691006
master <lehs9@DESKTOP-7ADTNUI>
2020-09-18 13:00:12 +0900

    인덱스처리

--- src/com/semi/member/controller/EnrollMemberEndServlet.java
+++ src/com/semi/member/controller/EnrollMemberEndServlet.java
@@ -36,17 +36,6 @@ public class EnrollMemberEndServlet extends HttpServlet {
 		//인코딩 처리
 		request.setCharacterEncoding("UTF-8");
 		
-		//관심분야 다중값을 전달함
-		/* 	인문사회 100
-			자연과학 200
-			정보통신 300
-			자기계발 400
-			문학 500 */
-		String[] interest=request.getParameterValues("interest");
-		
-		//오라클 DB에는 배열자료형을 문자형으로 변환해줘야함.
-		String interested=String.join(",",interest);
-		
 		String memberId=request.getParameter("id");
 		//회원가입정보(데이터)를 받아옴 (Member)
 		Member m=new Member();
@@ -66,7 +55,8 @@ public class EnrollMemberEndServlet extends HttpServlet {
 		m.setEmail(request.getParameter("email"));
 		m.setAgency(request.getParameter("telecom"));
 		m.setPhone(request.getParameter("phone"));
-	
+		
+		// 시퀀스 가져오기
 		int result=new MemberService().insertMember(m);
 		Member seq=new MemberService().getseq(memberId);
 		
@@ -74,11 +64,70 @@ public class EnrollMemberEndServlet extends HttpServlet {
 		int memberSeq=seq.getMemberNo();
 		System.out.println(memberSeq);
 		
-		int interestResult=new MemberService().insertInterest(memberSeq, interested);
+		//관심분야 다중값을 전달함
+		/* 	인문사회 100
+			자연과학 200
+			정보통신 300
+			자기계발 400
+			문학 500 */
+		
+		String[] interest=request.getParameterValues("interest");
+
+		System.out.println("interst : "+interest);
+			if(interest.length==1) {
+				System.out.println("interest index 갯수 1: "+interest[0]);
+				String interested=interest[0];
+				int interestResult=new MemberService().insertInterest(memberSeq, interested);
+			}
+			if(interest.length==2) {
+				String interested=interest[0];
+				int interestResult=new MemberService().insertInterest(memberSeq, interested);
+				String interested1=interest[1];
+				int interestResult1=new MemberService().insertInterest(memberSeq, interested1);
+			}
+			if(interest.length==3) {
+				String interested=interest[0];
+				int interestResult=new MemberService().insertInterest(memberSeq, interested);
+				String interested1=interest[1];
+				int interestResult1=new MemberService().insertInterest(memberSeq, interested1);
+				String interested2=interest[2];
+				int interestResult2=new MemberService().insertInterest(memberSeq, interested2);
+			}
+			if(interest.length==4) {
+				String interested=interest[0];
+				int interestResult=new MemberService().insertInterest(memberSeq, interested);
+				String interested1=interest[1];
+				int interestResult1=new MemberService().insertInterest(memberSeq, interested1);
+				String interested2=interest[2];
+				int interestResult2=new MemberService().insertInterest(memberSeq, interested2);
+				String interested3=interest[3];
+				int interestResult3=new MemberService().insertInterest(memberSeq, interested3);
+			}
+			if(interest.length==5) {
+				String interested=interest[0];
+				int interestResult=new MemberService().insertInterest(memberSeq, interested);
+				String interested1=interest[1];
+				int interestResult1=new MemberService().insertInterest(memberSeq, interested1);
+				String interested2=interest[2];
+				int interestResult2=new MemberService().insertInterest(memberSeq, interested2);
+				String interested3=interest[3];
+				int interestResult3=new MemberService().insertInterest(memberSeq, interested3);
+				String interested4=interest[4];
+				int interestResult4=new MemberService().insertInterest(memberSeq, interested4);
+			}
+			/*
+			 * for(int i=0;i<interest.length;i++) { String interested=interest[i]; int
+			 * interestResult=new MemberService().insertInterest(memberSeq, interested); }
+			 */
+			/*오라클 DB에는 배열자료형을 문자형으로 변환해줘야함.
+			String interested=String.join(",",interest);*/
+				
+		
 		
 		String msg="";
 		String loc="/";
-		if(result>0&&interestResult>0) {
+		/*if(result>0&&interestResult>0) {*/
+		if(result>0) {
 			msg="회원가입성공";
 		}else {
 			msg="회원가입실패";
